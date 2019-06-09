554272b0217b36eae3c33d163eb0ab96b4ea1a7a
Andrew <healeycodes@gmail.com>
2019-06-09 16:17:48 +0100

    Fix listening message when app launches

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const { app } = require('./app');
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function() {
   const port = listener.address().port;
-  console.log(`Calorie King listening on port ' + ${port}\n\nClick me if you're DEVing: http://localhost:${port}`);
+  console.log(`Calorie King listening on port ${port}\n\nClick me if you're DEVing: http://localhost:${port}`);
 });
